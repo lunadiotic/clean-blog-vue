36b7f37e5320334aa72c059df1e50369dcb92ed3
lunadiotic <ahmadhakim404@gmail.com>
2021-03-20 23:34:44 +0700

    install bootstrap jquery popper.js fontawesome

--- src/main.js
+++ src/main.js
@@ -3,4 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+/* Bootstrap CSS */
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+/* jQuery */
+import JQuery from 'jquery'
+window.$ = JQuery
+
+/* Fontawesome */
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
 createApp(App).use(store).use(router).mount('#app')
